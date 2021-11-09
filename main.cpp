b2089cac2cb45e8a3704ffe4b4739fcb615e09b5
Vectari <93955642+Vectari@users.noreply.github.com>
2021-11-09 01:42:54 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Od 4 do -1" << endl;
+   for (int i=4; i>=-1; i--)
+    {
+       cout << i << endl;
+    }
+    cout << "OD 1 do 10" << endl;
+    for (int i=1; i<=10; i++)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
